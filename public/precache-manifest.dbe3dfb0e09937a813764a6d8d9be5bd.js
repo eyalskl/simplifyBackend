04e80908d4678e5a42c6fb0c349635a18f1ef331
Eyal Barkai <eyalskl18@gmail.com>
2020-07-26 12:36:46 +0300

    building for production

---
+++ public/precache-manifest.dbe3dfb0e09937a813764a6d8d9be5bd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b05ab7a24b3ecd7ce29",
+    "url": "/css/app.297dd424.css"
+  },
+  {
+    "revision": "dc6d6355483388b3238e",
+    "url": "/css/chunk-vendors.bab20854.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "d2e0f0ab98ce5245bd8c3d089116f954",
+    "url": "/img/easyUse.d2e0f0ab.png"
+  },
+  {
+    "revision": "99abab8e8f786968edcdea0c64a92770",
+    "url": "/img/hero-temp.99abab8e.jpg"
+  },
+  {
+    "revision": "f0ff63459469783a5ee09eb364111383",
+    "url": "/img/iphone.f0ff6345.png"
+  },
+  {
+    "revision": "feb9a38faefec1a339e9ad77b4caebe0",
+    "url": "/img/loadingAnimation.feb9a38f.svg"
+  },
+  {
+    "revision": "5f76fa5c0ccdcf38989ec05bc23a5fca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b05ab7a24b3ecd7ce29",
+    "url": "/js/app.b5b38d8d.js"
+  },
+  {
+    "revision": "dc6d6355483388b3238e",
+    "url": "/js/chunk-vendors.1bfeadff.js"
+  },
+  {
+    "revision": "26a82ee6e29e128cc69265f30ef3a4ed",
+    "url": "/logo.png"
+  },
+  {
+    "revision": "55eacd35b1075a9c84fb9fe07a7e0fa1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ public/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.dbe3dfb0e09937a813764a6d8d9be5bd.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "web-it"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
